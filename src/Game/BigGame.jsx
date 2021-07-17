1bb5acd24cd430bfa9752bf380f8f8186b3a7c3d
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2021-07-17 05:57:35 +0500

    counter

--- src/Game/BigGame.jsx
+++ src/Game/BigGame.jsx
@@ -7,20 +7,57 @@ const BigGame = () => {
   // Потом сравниваем два списка ответов рядом
 
   const [hidden, setHidden] = useState(false);
+  const [leftScore, setLeftScope] = useState(0);
+  const [rightScore, setRightScope] = useState(0);
+  const [leftNumbers, setLeftNumbers] = useState([]);
+  const [rightNumbers, setRightNumbers] = useState([]);
+  const numberOfQuestions = 6;
+
+  let leftBlocks = [];
+  let rightBlocks = [];
+  for (let i = 0; i < numberOfQuestions; i++) {
+    leftBlocks.push(<div className="answersField__answersContainer">
+      <div className="answersField__answers--answer unCover">
+                                    <span className="answerText">
+                                        <input className="text"/>
+                                        <input onChange={(e) => {
+                                          let leftNew = [...leftNumbers];
+                                          leftNew[i] = Number(e.target.value);
+                                          setLeftNumbers(leftNew);
+                                          setLeftScope(leftNew.reduce((a, b) => a + b, 0));
+                                        }} className="number"/>
+                                    </span>
+      </div>
+    </div>)
+
+    rightBlocks.push(<div className="answersField__answersContainer">
+      <div className="answersField__answers--answer unCover">
+                                    <span className="answerText">
+                                        <input className="text"/>
+                                        <input onChange={(e) => {
+                                          let rightNew = [...rightNumbers];
+                                          rightNew[i] = Number(e.target.value);
+                                          setRightNumbers(rightNew);
+                                          setRightScope(rightNew.reduce((a, b) => a + b, 0));
+                                        }} className="number"/>
+                                    </span>
+      </div>
+    </div>)
+  }
 
   return (
     <div className="gameBoard">
       <div className="totalScore">
         <div className="totalScore__window">
           {/*Сюда складываются очки из плашечек*/}
-          <span className="totalScore__window--value">200</span>
+          <span className="totalScore__window--value">{leftScore + rightScore}</span>
         </div>
       </div>
       <div className="questionsField">
         <div className="leftBandScore">
           <div className="leftBandScore__window">
             {/*Сюда складываются очки первой команды*/}
-            <span className="leftBandScore__window--value">111</span>
+            <span className="leftBandScore__window--value">{leftScore}</span>
           </div>
         </div>
 
@@ -28,106 +65,11 @@ const BigGame = () => {
           <div className="answersField__answers bigGameField">
 
             <div className="answersField__answers--bigGameLeft">
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
+              {leftBlocks}
             </div>
 
             <div className="answersField__answers--bigGameLeft">
-              {/* Ответы второго игрока */}
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
-              <div className="answersField__answersContainer">
-                <div className="answersField__answers--answer unCover">
-                                    <span className="answerText">
-                                        <input className="text"/>
-                                        <input className="number"/>
-                                    </span>
-                </div>
-              </div>
+              {rightBlocks}
             </div>
           </div>
         </div>
@@ -135,7 +77,7 @@ const BigGame = () => {
         <div className="rightBandScore">
           <div className="rightBandScore__window">
             {/*Сюда складываются очки второй команды*/}
-            <span className="rightBandScore__window--value">222</span>
+            <span className="rightBandScore__window--value">{rightScore}</span>
           </div>
         </div>
       </div>
